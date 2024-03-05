0ec567e2fc1757338bad7cb3db6e7847f6a51860
Sennett Lau <laub1199@gmail.com>
2024-03-06 00:39:29 +0800

    updated asian-raise page

--- src/app/story/asian-raise/page.tsx
+++ src/app/story/asian-raise/page.tsx
@@ -1,9 +1,144 @@
+import StoryContainer from '@/components/story/StoryContainer'
+
 const AsianRaise = () => {
   return (
-    <div>
-      <h1>Asian Raise</h1>
-    </div>
-
+    <StoryContainer
+      title='Why PFP. Why Asia.'
+      title2='Why, PFPAsia.'
+      pfp='/assets/pfps/dan-4.png'
+      alt='Dan'
+    >
+      <p>
+        2024 is the year about Asian culture is ready to appear in Global NFT
+        market, and many Asian culture-themed NFTs are ready to show up to the
+        international stage.
+      </p>
+      <p>
+        From my personal view, I highly recommended that everyone can have more
+        time on looking how Asian projects perform in 2024 and give Asian
+        projects more opportunities to show their leadership.
+      </p>
+      <p>
+        Make a sophisticated decision, find a cultural identity that you
+        appreciate, and think how to support it according to your own situation.
+      </p>
+      <p>The team of PFPAsia has a very deep understanding of Asian culture.</p>
+      <p>
+        According to the team, the destiny for PFPAsia, is calling all Web3
+        player can enjoy our Asian cultures. PFPAsia will do massive works on
+        this journey with their artist team, and building the Asia's most iconic
+        PFPs, through the high quality attributes and traits designs.
+      </p>
+      <p>
+        This collection use the Asian tradition key-color as the base color
+        theme, and learn from many Asian iconic visualized style in social
+        media.
+      </p>
+      <p>
+        Including more than 200 men's and women's hairstyles, thousands of
+        outfits and costumes, more than 100 emotional expressions, and also
+        numerous style from different career, traditional or urban careers.
+      </p>
+      <p>
+        In the design of the attributes, hundreds of Asian legends were
+        modernized.
+      </p>
+      <p>
+        Nvuwa also redesigning uniforms according to various traditional Western
+        cultures,
+      </p>
+      <p>such as nuns wearing silk with Chinese traditional patterns...</p>
+      <p>
+        The most distressing thing for the team is that there has been a
+        extremely bear market, particular the NFT market.
+      </p>
+      <p>
+        Even NFTs are for unique and non-fungibles, but majority NFT asset is
+        lacks on exclusivity, they blurring the boundaries between different
+        standards such as erc721 and erc1155 Sometimes, the only difference
+        between NFTs and erc20 is that the minimum unit cannot be lower than 1.
+      </p>
+      <p>
+        Quality is the basic for PFPAsia. The original vision was to connect
+        creators with influential JPEGs, rather than focusing on such as "total
+        trading volume,” "mint price," "market maker draw a beautiful line,”
+        Nvuwa will not create a “Shadow Mint" strategy with high percentage of
+        control by team.
+      </p>
+      <p>
+        Focusing on solving more important issues, "resource allocation",
+        "collective development", "fair and transparent contributions",
+        "transition from simple speculation to investment", "not just an
+        economic model, but a business model", "mutual learning and growth",
+        "sustainable and lasting benefits for all stakeholders".
+      </p>
+      <p>
+        Although it's still a winter for NFTs, it's not like many people said
+        that "NFTs are dead".
+      </p>
+      <p>
+        In fact, the bad NFTs are died, and the bad people left. Bad management
+        gone. But NFTs will never going to die.
+      </p>
+      <p>
+        The PFPAsia team said they still believe this is one of the best
+        opportunities to awaken cultural awareness and create brand loyalty.
+      </p>
+      <p>
+        At least for me, NFTs are the best way to manage intellectual-property
+        assetswith communities and stakeholder economies.
+      </p>
+      <p>
+        The team recognized that a iconic portrait had great potential to build
+        a vibrant community. Behind the PFP is a beautiful combination of
+        brands, intellectual property (IP), logos, voting rights, income and
+        dividends, memberships, luxury goods, art, fashion and even digi-twins.
+      </p>
+      <p>PFPAsia is here, to make these ideas come true.</p>
+      <p>Let's reveal the artist's workflow and daily routine.</p>
+      <p>
+        For example, the AI and HI system. In the Telegram community, there was
+        a question about whether Dan uses AI or not.
+      </p>
+      <p>
+        In fact, many people have been wondering, because it is very difficult
+        for manpower,achieve such a huge workload, If every PFP is totally
+        handworks.
+      </p>
+      <p>
+        but AI is impossible to build a decent 10k collection right now. Anyone
+        who have tried AI, will understand the unstable results in out of
+        control, Can not use them to a 10k PFP project directly.
+      </p>
+      <p>
+        Even this is challenged, we were moved by the PFPAsia team, They have a
+        super high transparency and honesty. In the Telegram community, Nvuwa
+        answered these questions.
+      </p>
+      <p>
+        They use their creativity to feed an AI model. And they treat AI as tool
+        to inspire them. AI is a mirror, or I say it is a Telescope to observe
+        different characters.
+      </p>
+      <p>
+        And that's just the first step, which will eliminate 94% of the original
+        result.
+      </p>
+      <p>
+        The team use multiple tools to improve quality. In this step, PFPAsia’s
+        team used about 8 tools to fixing their quality with the human eyes.
+      </p>
+      <p>
+        In the third step, they will use hand-drawing, PhotoShop, Adobe
+        Illustrator, to do more details. After this, they has more than 10
+        optional steps to get the ultimate result they wanted.
+      </p>
+      <p>
+        Why so complex? Nvuwa told us, this is the only way to deal with AI and
+        Human Intelligence. A perfect sweet spot. In this market, there are no
+        projects are using this method.
+      </p>
+    </StoryContainer>
   )
 }
 
