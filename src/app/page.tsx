85102c33601185b8366869f362bdb70120f6361f
Sennett Lau <laub1199@gmail.com>
2024-03-05 16:18:40 +0800

    added background and content for index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className='w-full flex-1 flex justify-center items-center'>
-      <h1>Home</h1>
+    <div className='w-full flex-1 flex justify-center items-center bg-index bg-no-repeat bg-cover bg-center'>
+      <Image src='/assets/PFPASIA.png' alt='PFPASIA' width={1246} height={228} className='max-w-full' />
     </div>
   )
 }
