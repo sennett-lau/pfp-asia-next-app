1cd8a3424948472f87e41bebeb573fcb7dd29677
Sennett Lau <laub1199@gmail.com>
2024-03-21 19:15:01 +0800

    added discord oauth

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,25 +1,56 @@
+'use client'
+
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 const Profile = () => {
+  const session = useSession()
+
+  const [discordUsername, setDiscordUsername] = useState('')
+  const [discordAvatar, setDiscordAvatar] = useState('')
+
+  useEffect(() => {
+    if (session.data) {
+      if (session.data.user?.name && session.data.user?.image) {
+        setDiscordUsername(session.data.user.name)
+        setDiscordAvatar(session.data.user.image)
+      }
+    }
+  }, [session])
+
   return (
     <div className='max-w-11xl mx-auto w-full px-4 flex md:pb-0 justify-center'>
       <div className='flex flex-col gap-4 bg-white rounded-lg shadow-lg px-4 py-8 w-full md:w-1/2'>
         <div className='custom-connect-button'>
           <ConnectButton showBalance={false} />
         </div>
-        <button>
-          <div className='flex items-center justify-center w-full h-20 bg-discord-500 text-white rounded-lg hover:scale-105 transform transition-all duration-300'>
-            Connect Discord
-            <Image
-              src='/assets/icons/discord-white.svg'
-              alt='Discord'
-              width={20}
-              height={20}
-              className='ml-2'
-            />
-          </div>
-        </button>
+        {discordUsername && discordAvatar ? (
+          <button onClick={() => signOut()}>
+            <div className='flex items-center justify-center w-full h-20 bg-discord-500 text-white rounded-lg hover:scale-105 transform transition-all duration-300'>
+              Connected as {discordUsername}
+              <img
+                src={discordAvatar}
+                alt='Discord'
+                className='ml-2 w-8 h-8 rounded-full'
+              />
+            </div>
+          </button>
+        ) : (
+          <button onClick={() => signIn('discord')}>
+            <div className='flex items-center justify-center w-full h-20 bg-discord-500 text-white rounded-lg hover:scale-105 transform transition-all duration-300'>
+              Connect Discord
+              <Image
+                src='/assets/icons/discord-white.svg'
+                alt='Discord'
+                width={20}
+                height={20}
+                className='ml-2'
+              />
+            </div>
+          </button>
+        )}
       </div>
     </div>
   )
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,12 @@
 
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
 import { WagmiProvider } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 
 const config = getDefaultConfig({
-  appName: 'PFPAsia ',
+  appName: 'PFPAsia',
   projectId: '42ce92d52845824ac2f2f05bbb5b1b53',
   chains: [mainnet],
 })
@@ -22,9 +23,11 @@ const Providers = (props: Props) => {
 
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
-      </QueryClientProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>{children}</RainbowKitProvider>
+        </QueryClientProvider>
+      </SessionProvider>
     </WagmiProvider>
   )
 }
