7ffa0c9c6f3306dcb586ef4b2b4bbe00e8dc6038
Sennett Lau <laub1199@gmail.com>
2024-03-21 16:36:16 +0800

    added rainbowkit related for wallet connection

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,15 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Image from 'next/image'
 
 const Profile = () => {
   return (
     <div className='max-w-11xl mx-auto w-full px-4 flex md:pb-0 justify-center'>
       <div className='flex flex-col gap-4 bg-white rounded-lg shadow-lg px-4 py-8 w-full md:w-1/2'>
+        <div className='custom-connect-button'>
+          <ConnectButton showBalance={false} />
+        </div>
         <button>
-          <div className='flex items-center justify-center w-full h-20 bg-primary-500 text-white rounded-lg'>
-            Connect Wallet
-          </div>
-        </button>
-
-        <button>
-          <div className='flex items-center justify-center w-full h-20 bg-discord-500 text-white rounded-lg'>
+          <div className='flex items-center justify-center w-full h-20 bg-discord-500 text-white rounded-lg hover:scale-105 transform transition-all duration-300'>
             Connect Discord
             <Image
               src='/assets/icons/discord-white.svg'
